f71f1fd92080c416c5b510ded10a6ac6799212fe
James Morris <blondfro@gmail.com>
2021-04-16 15:40:25 -0500

    installed bootstrap for styling.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from "popper.js";
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
