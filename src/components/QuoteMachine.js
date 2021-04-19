67d5ba4f5c307893caf53fe81366ea8137e8b199
James Morris <blondfro@gmail.com>
2021-04-19 14:50:28 -0500

    created the basic functionality.

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {quoteData} from '../data/quoteData';
+
 
 function QuoteMachine() {
+    const [authorQuotes, setAuthorQuotes] = useState(quoteData);
+    const [currAuthor, setCurrAuthor] = useState({
+        author: '',
+        quote: ''
+    })
+
+    useEffect(()=> {
+        // on load get an initial quote.
+        getQuote()
+
+    }, []);
+
+
+    const getQuote = () => {
+        let num = Math.floor(Math.random() * authorQuotes.length);
+        console.log('the number is: ', num);
+        let currentVal = authorQuotes[num];
+        console.log('current quote: ', currentVal)
+        setCurrAuthor({...currentVal});
+    }
+
     return (
         <div>
-
+            <h1>{currAuthor.author}</h1>
+            <p>{currAuthor.quote}</p>
+            <button onClick={getQuote}>Next</button>
         </div>
     )
 }
---
+++ src/data/quoteData.js
@@ -0,0 +1,34 @@
+export const quoteData = [
+    {
+        "author": "Malcolm X",
+        "quote": "It is incorrect to classify the revolt of the Negroes as simply a racial conflict of black against white.. Rather we are today seeing a global rebellion of the oppressed against the oppressor, the exploited against the exploited…"
+    },
+    {
+        "author": "Marcus Garvey",
+        "quote": "A people without the knowledge of their past history, origin and culture is like a tree without roots."
+    },
+    {
+        "author": "W. E. B. Du Bois",
+        "quote": "Between me and the other world there is ever an unasked question: ... How does it feel to be a problem? ... One ever feels his two-ness,–an American, a Negro; two souls, two thoughts, two unreconciled strivings; two warring ideals in one dark body, whose dogged strength alone keeps it from being torn asunder ..."
+    },
+    {
+        "author": "Sojourner Truth",
+        "quote": "Jesus loved me! I knew it, – I felt it! Jesus was my Jesus. Jesus would love me always. I didn’t dare tell nobody; ‘t was a great secret. Everything had been got away from me that I ever had; an’ I thought that ef I let white folks know about this, maybe they’d get Him away, so I said, ‘I’ll keep this close. I won’t let any one know."
+    },
+    {
+        "author": "Dr. Martin Luther King",
+        "quote": "And this is one of the tragedies of human nature. So we begin to love our enemies and love those persons that hate us whether in collective life or individual life by looking at ourselves."
+    },
+    {
+        "author": "Frederick Douglass",
+        "quote": "Though conscious of the difficulty of learning without a teacher, I set out with high hope, and a fixed purpose, at whatever cost of trouble, to learn how to read."
+    },
+    {
+        "author": "Booker T. Washington",
+        "quote": "Nothing ever comes to me, that is worth having, except as a result of hard work."
+    },
+    {
+        "author": "Harriet Tubman",
+        "quote": "When I found I had crossed that line, I looked at my hands to see if I was the same person. There was such a glory over everything; the sun came like gold through the trees, and over the fields, and I felt like I was in Heaven."
+    }
+]
